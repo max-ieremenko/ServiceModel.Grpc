ca7406f09c1345a51b9b2055c463ed44136d9cf6
Max Ieremenko <max.ieremenko@gmail.com>
2020-04-21 23:24:29 +0200

    1.0.0

--- Sources/GlobalAssemblyInfo.cs
+++ Sources/GlobalAssemblyInfo.cs
@@ -9,4 +9,4 @@
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-pre2")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
