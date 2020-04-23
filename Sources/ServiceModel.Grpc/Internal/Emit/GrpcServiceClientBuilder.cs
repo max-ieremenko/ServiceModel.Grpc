4aedcc1ec9a74f1afd0bdbbc5135d50a0302326f
Max Ieremenko <max.ieremenko@gmail.com>
2020-04-23 18:42:13 +0200

    standard Apache licence header

--- Sources/ServiceModel.Grpc/Internal/Emit/GrpcServiceClientBuilder.cs
+++ Sources/ServiceModel.Grpc/Internal/Emit/GrpcServiceClientBuilder.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// <copyright>
+// Copyright 2020 Max Ieremenko
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -236,12 +252,12 @@ namespace ServiceModel.Grpc.Internal.Emit
             body.Emit(OpCodes.Ldnull); // var host = null
             body.Emit(OpCodes.Ldloc_0); // options
             body.Emit(OpCodes.Ldloc_2); // message
-            
+
             if (message.IsAsync)
             {
                 // CallInvoker.AsyncUnaryCall(...Method, null, context, value);
                 body.Emit(OpCodes.Callvirt, typeof(CallInvoker).InstanceMethod(nameof(CallInvoker.AsyncUnaryCall)).MakeGenericMethod(message.RequestType, message.ResponseType));
-                
+
                 PushCallContext(body, message);
 
                 if (message.ResponseType.IsGenericType)
@@ -396,7 +412,7 @@ namespace ServiceModel.Grpc.Internal.Emit
             {
                 body.Emit(OpCodes.Ldloca_S, 1); // optionsBuilder
                 body.EmitLdarg(i + 1); // parameter
-                
+
                 var withMethodName = "With" + message.Parameters[i].ParameterType.Name;
                 body.Emit(OpCodes.Call, typeof(CallOptionsBuilder).InstanceMethod(withMethodName)); // .With
                 body.Emit(OpCodes.Stloc_1);
