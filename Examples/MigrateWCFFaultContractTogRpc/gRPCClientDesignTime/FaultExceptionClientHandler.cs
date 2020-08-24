ec1a0f3b4b63f242508b213c77d9e90e9ec01754
Max Ieremenko <max.ieremenko@gmail.com>
2020-08-24 19:50:15 +0200

    extend examples with ClientDesignTime

---
+++ Examples/MigrateWCFFaultContractTogRpc/gRPCClientDesignTime/FaultExceptionClientHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel;
+using Contract;
+using ServiceModel.Grpc.Interceptors;
+
+namespace gRPCClientDesignTime
+{
+    internal sealed class FaultExceptionClientHandler : ClientErrorHandlerBase
+    {
+        protected override void ThrowOrIgnoreCore(ClientCallInterceptorContext context, ClientFaultDetail detail)
+        {
+            // handle ApplicationExceptionFaultDetail
+            if (detail.Detail is ApplicationExceptionFaultDetail appDetail)
+            {
+                throw new FaultException<ApplicationExceptionFaultDetail>(appDetail);
+            }
+
+            // handle InvalidOperationExceptionFaultDetail
+            if (detail.Detail is InvalidOperationExceptionFaultDetail opDetail)
+            {
+                throw new FaultException<InvalidOperationExceptionFaultDetail>(opDetail);
+            }
+
+            // ignore other errors
+        }
+    }
+}
---
+++ Examples/MigrateWCFTogRpc/gRPCClientDesignTime/MyGrpcServices.cs
@@ -0,0 +1,10 @@
+﻿using Contract;
+using ServiceModel.Grpc.DesignTime;
+
+namespace gRPCClientDesignTime
+{
+    [ImportGrpcService(typeof(IPersonService))] // configure ServiceModel.Grpc.DesignTime to generate a source code for IPersonService proxy
+    internal static partial class MyGrpcServices
+    {
+    }
+}
