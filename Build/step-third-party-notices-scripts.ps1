c66207977c97dad7c3330d0d4437a43c1066072c
Max Ieremenko <max.ieremenko@gmail.com>
2021-03-27 19:07:39 +0100

    build: handle exit codes

--- Build/step-third-party-notices-scripts.ps1
+++ Build/step-third-party-notices-scripts.ps1
@@ -4,20 +4,26 @@ function Write-ThirdPartyNotices($appNames, $sources, $repository, $out) {
     $source = $sources | ForEach-Object {"-source", $_}
     $outTemp = Join-Path $out "Temp"
 
-    ThirdPartyLibraries update `
-        -appName $appName `
-        $source `
-        -repository $repository
-  
-    ThirdPartyLibraries validate `
-        -appName $appName `
-        $source `
-        -repository $repository
+    Exec {
+        ThirdPartyLibraries update `
+            -appName $appName `
+            $source `
+            -repository $repository
+    }
 
-    ThirdPartyLibraries generate `
-        $generateAppNames `
-        -repository $repository `
-        -to $outTemp
+    Exec {
+        ThirdPartyLibraries validate `
+            -appName $appName `
+            $source `
+            -repository $repository
+    }
+
+    Exec {
+        ThirdPartyLibraries generate `
+            $generateAppNames `
+            -repository $repository `
+            -to $outTemp
+    }
 
     $licenseFile = $appName + "ThirdPartyNotices.txt"
     Move-Item (Join-Path $outTemp "ThirdPartyNotices.txt") (Join-Path $out $licenseFile) -Force
