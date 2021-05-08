4b7ff6c86d165758ed656b4294318ac63809fd3f
Max Ieremenko <max.ieremenko@gmail.com>
2021-05-08 12:59:13 +0200

    MessagePackMarshaller example: use MessagePack attributes

--- Examples/MessagePackMarshaller/Contract/Person.cs
+++ Examples/MessagePackMarshaller/Contract/Person.cs
@@ -1,18 +1,18 @@
 ﻿using System;
-using System.Runtime.Serialization;
+using MessagePack;
 
 namespace Contract
 {
-    [DataContract]
+    [MessagePackObject]
     public class Person
     {
-        [DataMember(Order = 1)]
+        [Key(1)]
         public string Name { get; set; }
 
-        [DataMember(Order = 2)]
+        [Key(2)]
         public DateTime BirthDay { get; set; }
 
-        [DataMember(Order = 3)]
+        [Key(3)]
         public string CreatedBy { get; set; }
     }
 }
