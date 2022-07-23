66b8b051df92c9fb5ab2708443f63a977649a3a0
max-ieremenko <>
2022-07-23 14:26:09 +0200

    example "Counter": upgrade to net6

--- Examples/grpc-dotnet-Counter/Client/Program.cs
+++ Examples/grpc-dotnet-Counter/Client/Program.cs
@@ -19,7 +19,7 @@ namespace Client
 
         public static async Task Main(string[] args)
         {
-#if !NET5_0
+#if NETCOREAPP3_1
             ServiceModel.Grpc.GrpcChannelExtensions.Http2UnencryptedSupport = true;
 #endif
 
