7dfadf25e6cc21701db2eb03da0691d94e4cc57f
max-ieremenko <>
2022-07-23 14:31:31 +0200

    ci build: remove net5 from sdktest

--- Build/scripts/Get-DockerDotNetImageSdk.ps1
+++ Build/scripts/Get-DockerDotNetImageSdk.ps1
@@ -1,16 +1,12 @@
 function Get-DockerDotNetImageSdk {
     param (
         [Parameter(Mandatory = $true)]
-        [ValidateSet("netcoreapp3.1", "net5.0", "net6.0")]
+        [ValidateSet("netcoreapp3.1", "net6.0")]
         $DotNet
     )
     
     if ($DotNet -eq "netcoreapp3.1") {
         return "mcr.microsoft.com/dotnet/core/sdk:3.1"
-    }
-
-    if ($DotNet -eq "net5.0") {
-        return "mcr.microsoft.com/dotnet/sdk:5.0"
     }
 
     return "mcr.microsoft.com/dotnet/sdk:6.0"
