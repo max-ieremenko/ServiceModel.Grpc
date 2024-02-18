bd482ddcf90d1ceca195a3730cbf442bac9b2ac2
max-ieremenko <max.ieremenko@gmail.com>
2024-02-18 13:58:05 +0100

    error handling examples: use WebApplication

--- Examples/ErrorHandling/ServerAspNetHost/Program.cs
+++ Examples/ErrorHandling/ServerAspNetHost/Program.cs
@@ -1,23 +1,43 @@
 ﻿using System.Threading.Tasks;
 using Contract;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Service;
+using ServiceModel.Grpc.Interceptors;
 
 namespace ServerAspNetHost;
 
 public static class Program
 {
-    public static Task Main(string[] args)
+    public static Task Main()
     {
-        return Host
-            .CreateDefaultBuilder()
-            .ConfigureWebHostDefaults(webBuilder =>
+        var builder = WebApplication.CreateBuilder();
+
+        builder.Services.AddSingleton<IServerErrorHandler>(_ =>
+        {
+            // combine application and unexpected handlers into one handler
+            var collection = new ServerErrorHandlerCollection(
+                new ApplicationExceptionServerHandler(),
+                new UnexpectedExceptionServerHandler());
+
+            return collection;
+        });
+
+        builder.Services
+            .AddServiceModelGrpc(options =>
             {
-                webBuilder.UseStartup<Startup>();
-                webBuilder.UseKestrel(o => o.ListenLocalhost(ServiceConfiguration.ServiceModelGrpcPort, l => l.Protocols = HttpProtocols.Http2));
-            })
-            .Build()
-            .RunAsync();
+                options.DefaultErrorHandlerFactory = serviceProvider => serviceProvider.GetRequiredService<IServerErrorHandler>();
+            });
+
+        builder.WebHost.ConfigureKestrel(options => options.ListenLocalhost(ServiceConfiguration.ServiceModelGrpcPort, l => l.Protocols = HttpProtocols.Http2));
+
+        var app = builder.Build();
+        
+        app.UseRouting();
+        app.MapGrpcService<DebugService>();
+
+        return app.RunAsync();
     }
 }
