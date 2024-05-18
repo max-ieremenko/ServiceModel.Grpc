ec5fa38edaa5b7e9ac17d6e9f4bdb8055303e269
max-ieremenko <max.ieremenko@gmail.com>
2024-05-18 11:03:21 +0200

    test BlazorApp example in publish mode

--- Examples/BlazorApp/test-configuration.ps1
+++ Examples/BlazorApp/test-configuration.ps1
@@ -2,15 +2,16 @@
     Solution      = "BlazorApp.sln"
     Configuration = "Release"
     Platform      = "Linux"
+    BuildMode     = "Publish"
 
     Tests         = @(
         , @( 
             @{
-                App  = "Server/bin/Release/net8.0/BlazorApp.Server.dll"
+                App  = "Server/bin/Release/net8.0/publish/BlazorApp.Server.dll"
                 Port = 5000
             }
             @{ App = "Invoke-WebRequest http://localhost:5000/IWeatherForecastService/GetForecasts -Method Post -ContentType application/grpc-web-text" }
-			@{ App = "ConsoleClient/bin/Release/net8.0/ConsoleApp.Client.dll" }
+			@{ App = "ConsoleClient/bin/Release/net8.0/publish/ConsoleApp.Client.dll" }
         )     
     )
 }
