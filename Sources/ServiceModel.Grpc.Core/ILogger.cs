9c0952c85f2591f54900e16bf1deefd64b493c55
max-ieremenko <max.ieremenko@gmail.com>
2024-07-06 13:46:29 +0200

    fix nullability

--- Sources/ServiceModel.Grpc.Core/ILogger.cs
+++ Sources/ServiceModel.Grpc.Core/ILogger.cs
@@ -26,19 +26,19 @@ public interface ILogger
     /// </summary>
     /// <param name="message">Format string of the log message.</param>
     /// <param name="args">An objects array that contains zero or more objects to format.</param>
-    void LogError(string message, params object[] args);
+    void LogError(string message, params object?[] args);
 
     /// <summary>
     /// Formats and writes a warning log message.
     /// </summary>
     /// <param name="message">Format string of the log message.</param>
     /// <param name="args">An objects array that contains zero or more objects to format.</param>
-    void LogWarning(string message, params object[] args);
+    void LogWarning(string message, params object?[] args);
 
     /// <summary>
     /// Formats and writes a debug log message.
     /// </summary>
     /// <param name="message">Format string of the log message.</param>
     /// <param name="args">An objects array that contains zero or more objects to format.</param>
-    void LogDebug(string message, params object[] args);
+    void LogDebug(string message, params object?[] args);
 }
