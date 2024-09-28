5c7fcc6750a74da1bb5cbf57aa24e04a996e2ebc
max-ieremenko <max.ieremenko@gmail.com>
2024-09-28 09:19:00 +0200

    MessagePackMarshaller.AOT example

---
+++ Examples/MessagePackMarshaller.AOT/Contract/Point.cs
@@ -0,0 +1,27 @@
+﻿using MessagePack;
+
+namespace Contract;
+
+[MessagePackObject]
+public sealed class Point
+{
+    public Point()
+    {
+    }
+
+    public Point(Number x, Number y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    [Key(1)]
+    public Number X { get; set; }
+
+    [Key(2)]
+    public Number Y { get; set; }
+
+    public static implicit operator Point((Number X, Number Y) value) => new(value.X, value.Y);
+
+    public override string ToString() => $"{X},{Y}";
+}
