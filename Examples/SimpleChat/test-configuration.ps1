3699b2d383b1d70b1e806fb14d4ae8d518d0d370
max-ieremenko <max.ieremenko@gmail.com>
2024-11-16 12:05:48 +0100

    SimpleChat: target framework net9.0

--- Examples/SimpleChat/test-configuration.ps1
+++ Examples/SimpleChat/test-configuration.ps1
@@ -1,16 +1,16 @@
 @{ 
-    Solution      = "SimpleChat.sln"
-    Configuration = "Release"
-    Platform      = "Linux"
-    BuildMode     = "Publish"
+    Solution      = 'SimpleChat.sln'
+    Configuration = 'Release'
+    Platform      = 'Linux'
+    BuildMode     = 'Publish'
 
     Tests         = @(
         , @( 
             @{
-                App  = "SimpleChat.Server/bin/Release/net8.0/publish/SimpleChat.Server.dll"
+                App  = 'SimpleChat.Server/bin/Release/publish/SimpleChat.Server.dll'
                 Port = 8080
             }
-			@{ App = "SimpleChat.Client.Tester/bin/Release/net8.0/publish/SimpleChat.Client.Tester.dll" }
+            @{ App = 'SimpleChat.Client.Tester/bin/Release/publish/SimpleChat.Client.Tester.dll' }
         )
     )
 }
