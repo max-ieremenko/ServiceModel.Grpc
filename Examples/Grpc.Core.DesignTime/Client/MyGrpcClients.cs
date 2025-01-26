54e20ca864d2db7e8901a0967b548f0655f0326f
max-ieremenko <max.ieremenko@gmail.com>
2025-01-26 13:11:17 +0100

    Grpc.Core examples

---
+++ Examples/Grpc.Core.DesignTime/Client/MyGrpcClients.cs
@@ -0,0 +1,8 @@
+﻿using Contract;
+using ServiceModel.Grpc.DesignTime;
+
+namespace Client;
+
+// configure ServiceModel.Grpc.DesignTime to generate a source code for ICalculator client proxy
+[ImportGrpcService(typeof(ICalculator))]
+internal static partial class MyGrpcClients;
---
+++ Examples/Grpc.Core.ReflectionEmit/Server/Services/ServerLoggerInterceptor.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Interceptors;
+using Microsoft.Extensions.Logging;
+
+namespace Server.Services;
+
+internal sealed class ServerLoggerInterceptor : Interceptor
+{
+    private readonly ILogger<ServerLoggerInterceptor> _logger;
+
+    public ServerLoggerInterceptor(ILogger<ServerLoggerInterceptor> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
+        TRequest request,
+        ServerCallContext context,
+        UnaryServerMethod<TRequest, TResponse> continuation)
+    {
+        LogCall(MethodType.Unary, context);
+        return continuation(request, context);
+    }
+
+    public override Task<TResponse> ClientStreamingServerHandler<TRequest, TResponse>(
+        IAsyncStreamReader<TRequest> requestStream,
+        ServerCallContext context,
+        ClientStreamingServerMethod<TRequest, TResponse> continuation)
+    {
+        LogCall(MethodType.ClientStreaming, context);
+        return continuation(requestStream, context);
+    }
+
+    public override Task ServerStreamingServerHandler<TRequest, TResponse>(
+        TRequest request,
+        IServerStreamWriter<TResponse> responseStream,
+        ServerCallContext context,
+        ServerStreamingServerMethod<TRequest, TResponse> continuation)
+    {
+        LogCall(MethodType.ServerStreaming, context);
+        return continuation(request, responseStream, context);
+    }
+
+    public override Task DuplexStreamingServerHandler<TRequest, TResponse>(
+        IAsyncStreamReader<TRequest> requestStream,
+        IServerStreamWriter<TResponse> responseStream,
+        ServerCallContext context,
+        DuplexStreamingServerMethod<TRequest, TResponse> continuation)
+    {
+        LogCall(MethodType.DuplexStreaming, context);
+        return continuation(requestStream, responseStream, context);
+    }
+
+    private void LogCall(MethodType methodType, ServerCallContext context)
+    {
+        _logger.LogInformation($"Starting {methodType} {context.Method}");
+    }
+}
