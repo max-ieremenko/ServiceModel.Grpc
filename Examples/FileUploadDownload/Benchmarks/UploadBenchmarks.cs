664e5c65ee5d16dc729479193692f36885756d9a
max-ieremenko <max.ieremenko@gmail.com>
2025-02-02 14:41:32 +0100

    simplify example FileUploadDownload

---
+++ Examples/FileUploadDownload/Benchmarks/UploadBenchmarks.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks;
+
+public class UploadBenchmarks : BenchmarkBase
+{
+    [Benchmark]
+    public Task Grpc() => GrpcClient.UploadFileAsync(FilePath, BufferSize, CancellationToken.None);
+
+    [Benchmark(Baseline = true)]
+    public Task Http() => HttpClient.UploadFileAsync(FilePath, BufferSize, CancellationToken.None);
+}
