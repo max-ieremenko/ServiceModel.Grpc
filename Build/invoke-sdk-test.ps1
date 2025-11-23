6651063fa8712c5dfaec4abed43baa81499ff3e6
max-ieremenko <>
2025-11-23 12:40:40 +0100

    update build dependencies

--- Build/invoke-sdk-test.ps1
+++ Build/invoke-sdk-test.ps1
@@ -1,5 +1,5 @@
 #Requires -Version "7.0"
-#Requires -Modules @{ ModuleName="InvokeBuild"; ModuleVersion="5.14.19" }
+#Requires -Modules @{ ModuleName="InvokeBuild"; ModuleVersion="5.14.22" }
 
 [CmdletBinding()]
 param (
